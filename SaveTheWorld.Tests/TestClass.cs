7d5310b6dd0ee5973806724e74cba1b7ab5c171b
Lasse Staal <lassestaal@gmail.com>
2017-11-23 14:12:20 +0100

    Initial project commit, trying to get travis up and running

---
+++ SaveTheWorld.Tests/TestClass.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace SaveTheWorld.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void AddTest()
+        {
+            SimpleClass simpleClass = new SimpleClass();
+            int result = simpleClass.Add(5, 7);
+            Assert.AreEqual(12, result);
+        }
+
+        [Test]
+        public void SubtractTest()
+        {
+            SimpleClass simpleClass = new SimpleClass();
+            int result = simpleClass.Sub(5, 3);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ SaveTheWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SaveTheWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class SimpleClass
+    {
+        public SimpleClass() { }
+
+        public int Add(int a, int b)
+        {
+            int x = a + b;
+            return x;
+        }
+
+        public int Sub(int a, int b)
+        {
+            int x = a - b;
+            return x;
+        }
+    }
+}
