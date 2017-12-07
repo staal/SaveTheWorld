78789dee64d4965ca1f7ec9c9ea27fc15c6c0bb0
staal <lassestaal@gmail.com>
2017-12-07 13:44:18 +0100

    Added Game class, which will be the main entry point to the game api

---
+++ SaveTheWorld/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveTheWorld
+{
+    class Game
+    {
+        private Board board;
+        private Random rng;
+
+        public Game(Board board, Int32 seed)
+        {
+            this.board = board;
+            rng = new Random(seed);
+        }
+
+        public Game(Board board)
+        {
+            this.board = board;
+            rng = new Random();
+        }
+    }
+}
